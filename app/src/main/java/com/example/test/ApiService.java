6c63ecfc3e087836cdc5baf7acab6d062e50b8fa
alex198 <abykovskih@gmail.com>
2023-01-28 00:05:25 +0300

    add Path request

--- app/src/main/java/com/example/test/ApiService.java
+++ app/src/main/java/com/example/test/ApiService.java
@@ -2,10 +2,11 @@ package com.example.test;
 
 import io.reactivex.rxjava3.core.Single;
 import retrofit2.http.GET;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface ApiService {
 
-    @GET("45717360")
-    Single<CardInfo> loadCardInfo();
+    @GET("{inputBin}")
+    Single<CardInfo> loadCardInfo(@Path("inputBin") int inputBin);
 }
